f5fd6e0dccfa16f42c4de63a8afdbd5b35670313
MilanDjogovic <milan.djogovic@gmail.com>
2020-09-30 13:55:13 +0200

    FIRST VERSION

--- main.js
+++ main.js
@@ -18,10 +18,39 @@ menuItem.forEach ((item) => {
 
 
 
+
 function toggle() {
     menuBtn.classList.toggle("open");
     menuItems.classList.toggle("open");
 }
+
+//CIRCLE BAR
+
+$(".circle_percent").each(function() {
+    var $this = $(this),
+		$dataV = $this.data("percent"),
+		$dataDeg = $dataV * 3.6,
+		$round = $this.find(".round_per");
+	$round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)");	
+	$this.append('<div class="circle_inbox"><span class="percent_text"></span></div>');
+	$this.prop('Counter', 0).animate({Counter: $dataV},
+	{
+		duration: 2000, 
+		easing: 'swing', 
+		step: function (now) {
+            $this.find(".percent_text").text(Math.ceil(now)+"%");
+        }
+    });
+	if($dataV >= 51){
+		$round.css("transform", "rotate(" + 360 + "deg)");
+		setTimeout(function(){
+			$this.addClass("percent_more");
+		},1000);
+		setTimeout(function(){
+			$round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)");
+		},1000);
+	} 
+});
 
 
 
